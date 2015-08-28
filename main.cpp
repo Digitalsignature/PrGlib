c5ead5bcade6c3d3170f065b2ab9735e33e5be4c
thang19xx <thang19xx@hotmail.com>
2015-08-28 10:31:04 +0700

    the first commit-generate strong primes for rsa

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "PrGlib.h"
+
+using namespace std;
+#define _TIME int starts,finishs;
+#define STARTS_TIME starts=clock();
+#define FINISHS_TIME finishs=clock(); cout<<(double)(finishs-starts)/CLOCKS_PER_SEC<<endl;
+
+int main()
+{
+    PrGlib genprime;
+    genprime.PrG_set_size(3072);
+    _TIME
+    while(true)
+    {
+        STARTS_TIME
+        genprime.PrG_renew();
+        cout<<genprime.PrG_generate_strong_prime().get_str()<<endl;
+        FINISHS_TIME
+    }
+    return 0;
+}
